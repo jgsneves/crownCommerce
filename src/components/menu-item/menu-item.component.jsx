8ba1855e8e5694cda27a306066ed2ed2a63e14de
jgsneves <jgsn.adv@gmail.com>
2020-03-20 01:43:25 -0300

    finalizacao de style e criação do directory component

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './menu-item.style.scss';
+
+const MenuItem = ({title, imageUrl, size}) => {
+    return(
+        <div style={{backgroundImage: `url(${imageUrl})`}} 
+        className={`${size} menu-item`}>
+            <div className='content'>
+                <h1 className='title'>{title}</h1>
+                <span className='subtitle'>compre agora</span>
+            </div>
+        </div>
+    );
+}
+
+export default MenuItem;
