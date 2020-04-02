22ea6aed32f2e0ab480cf0f10479368cf3bf6b69
jgsneves <jgsn.adv@gmail.com>
2020-04-02 17:08:57 -0300

    criacao do header do site

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component';
+import Header from '../src/components/header/header-component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage}/>
